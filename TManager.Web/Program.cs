27f37e22373c38eb8b9cd4e88bde6eb8eabe8cb9
Luis Forero <foluis@hotmail.com>
2025-10-20 10:18:12 -0600

    Add project files.

---
+++ TManager.Web/Program.cs
@@ -0,0 +1,38 @@
+using TManager.Web.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+//using TManager.Components;
+
+namespace TManager.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddCascadingAuthenticationState();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
